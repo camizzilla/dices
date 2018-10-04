f06bb4543f4c543de0b61a4fe2f10776e45968ed
Roberto Cazzaniga <rcazzaniga@urmetcns.it>
2018-10-04 14:25:35 +0200

    Sistema per aggiugere dadi

--- js/index.js
+++ js/index.js
@@ -3,68 +3,79 @@
 window.onload = () => {
   'use strict';
   let container = document.getElementById("container");
-  
+  let throwBtn = document.getElementById("throw-btn");
+
   class dice {
     constructor() {
       this.el = this.createDice();
-      this.timer  = 0;
+      this.timer = 0;
       this.timeDeceleration = 1;
       this.diceValue = 6;
-      this.diceFaces  = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;'];
-      
+      this.diceFaces = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;'];
+
       this.el.addEventListener("click", this.throw.bind(this), false);
-      
+
       this.init();
     }
     
-    init(){
+    init() {
       this.print(this.random());
     }
-
-    createDice(){
+    
+    createDice() {
       let div = document.createElement('div');
-      div.setAttribute('class', 'dice');                  
+      div.setAttribute('class', 'dice');
       return div;
     }
     
-    stop(){
-     clearTimeout(this.timer);
-      this.console(this.result)
-    };
+    console(res) {
+      return console.log(`Il risultato è ${res} 🎲`);
+    }
+    
+    stop() {
+      clearTimeout(this.timer);
+      this.timeDeceleration = 1;
+      this.console(this.result);
+    }
 
     random() {
       return Math.floor(Math.random() * this.diceValue);
     }
-    
+
     print(res) {
       this.el.innerHTML = this.diceFaces[res];
     }
-  
-     processing(){
+
+    processing() {
       let rand = this.random();
       this.print(rand);
       return rand + 1;
     }
-    
-    cycle(){
+
+    cycle() {
       this.timeDeceleration += 20;
       this.result = this.processing();
       this.timer = setTimeout(this.cycle.bind(this), this.timeDeceleration);
     }
-    
-    throw(){
-      setTimeout(this.stop.bind(this), 3000);
+
+    throw () {
+      setTimeout(this.stop.bind(this), 1000 * this.random());
       this.cycle();
     }
-    
-    console(res){
-      return console.log(`Il risultato è ${res} 🎲`);
-    }
-  }
-  
-  let dice1 = new dice();
-  let dice2 = new dice();
-  let dices = [dice1,dice2];
 
-container.appendChild(...dices);
-}
+  }
+  let dices = [];
+  let diceCreator = (nDice) => {
+    for (let i = 0; i < nDice; i++) {
+      let newDice = new dice();
+      container.appendChild(newDice.el);
+      dices.push(newDice);
+    }
+  };
+
+  diceCreator(3);
+  
+  let diceCycleFor = () => dices.forEach(dice => dice.throw());
+  throwBtn.addEventListener("click", () => diceCycleFor());
+
+};
